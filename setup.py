7bed787a449fe5b2f912054e391259aec6728f7f
eliasdesenv <38672688+eliasdesenv@users.noreply.github.com>
2018-04-30 21:20:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open('requirements.txt') as fp:
 
 
 setup(
-      name='apidoc',
-      version='1.1',
-      description='Devops Analytics',
+      name='gitlab-extraction',
+      version='1.0',
+      description='Gitlab Extraction for get metrics from development team',
       author='Elias Costa',
-      author_email='elias.costa@portalunimed.com.br',
-      py_modules=['run', 'apidoc'],
+      author_email='elias.desenv@gmail.com',
+      py_modules=['run', 'gitlab', 'api_resource', 'elasticsearch'],
       classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Programming Language :: Python :: 3.6'
       ],
       project_urls={
-            'Source': 'https://gitlab.portalunimed.com.br/microservices/apidocs-build',
+            'Source': 'https://github.com/eliasdesenv/gitlab-extraction/',
       },
       python_requires='>=3',
       packages=find_packages(),
